721e036a39c1ff7585a961490f13234b04234005
hotguy6pack <bowenrainyday@gmail.com>
2015-06-12 22:28:27 -0400

    basic interface

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var express = require("express");
 var app = require('express')();
 var http = require('http').Server(app);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
